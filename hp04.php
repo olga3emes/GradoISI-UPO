ac1701eeba4837e7c7d44b2c20115816cc2a8c1f
Olga M. Moreno Martín <olga3emes@gmail.com>
2019-11-18 09:55:15 +0100

    4-2

---
+++ hp04.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Harry Porlotes y El Malware de Fuego</title>
+ </head>
+ <body>
+<?php echo '<h1>Harry Porlotes y El Malware de Fuego</h1>'; ?>
+<?php print("<p>Un importante hackaton se celebra en la escuela y el portátil de un compañero de Harry, se avería debido a un ataque de Ransomwaremort, perdiendo así toda la saga Crepúsculo en epub que tenía guardada y las revistas de Marie-Claire de su abuela en PDF.</p>"
+           );?>
+ </body>
+</html>
